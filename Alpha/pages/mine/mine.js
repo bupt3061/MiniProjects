cd33d586cbcbd74b305e7e4ce8dc1088b6253560
bupt3061 <bupt3061@163.com>
2020-02-23 21:10:29 +0800

    2020/2/23 msg

--- Alpha/pages/mine/mine.js
+++ Alpha/pages/mine/mine.js
@@ -1,5 +1,5 @@
 // pages/mine/mine.js
-
+const app = getApp()
 var QR = require("../../utils/qrcode.js");
 
 Page({
@@ -8,6 +8,9 @@ Page({
    * 页面的初始数据
    */
   data: {
+    nickName: null,
+    avtarUrl: null,
+    rank: '../../img/',
     canvasHidden: false, //默认不让canvas二维码隐藏，否则不能生成二维码
     imagePath: "", //弹出框二维码显示图片地址
     inputValue: '',
@@ -15,72 +18,96 @@ Page({
     windowWidth: 0,
     initUrl: 'ILoveKangqianqian'
   },
-  async init() {
-    var initUrl = this.data.initUrl
-    let state = await this.createQrCode(initUrl, "qrCanvas", 60, 60)
-    if (state) {
-      let res = await this.canvasToTempImage()
-      var qrPath = res
-    }
-    console.log(qrPath)
+  /**
+   * 页面初始函数
+   */
+  init: function() {
+    const userInfo = app.globalData.userInfo
+    const nickName = userInfo.nickName
+    const avatarUrl = userInfo.avatarUrl
+    const ctb = userInfo.contribution
+    var rank = null
 
-    try {
-      const info = wx.getSystemInfoSync()
-      var windowWidth = info.windowWidth
-      var windowHeight = info.windowHeight
-      this.setData({
-        windowHeight: windowHeight,
-        windowWidth: windowWidth
-      })
-    } catch (e) {
-      // Do something when catch error
+    if(ctb <= 20) {
+      rank = this.data.rank + 'v1.png'
+    } else if(ctb <= 100) {
+      rank = this.data.rank + 'v2.png'
+    } else {
+      rank = this.data.rank + 'v3.png'
     }
 
-    const ctx = wx.createCanvasContext('myCanvas')
-    ctx.drawImage('../../img/post.jpg', 0, 0, windowWidth, windowHeight)
-    ctx.rect(windowWidth - 85, windowHeight - 105, 60, 60)
-    ctx.setFillStyle('red')
-    ctx.fill()
-    ctx.drawImage(qrPath, windowWidth - 85, windowHeight - 105, 60, 60)
-    ctx.draw()
-  },
-  createQrCode: function (url, canvasId, cavW, cavH) {
-    return new Promise((resolve, reject) => {
-      //调用插件中的draw方法，绘制二维码图片
-      QR.api.draw(url, canvasId, cavW, cavH);
-      resolve(true)
+    this.setData({
+      nickName: nickName,
+      avatarUrl: avatarUrl,
+      rank: rank
     })
   },
+  // async init() {
+  //   var initUrl = this.data.initUrl
+  //   let state = await this.createQrCode(initUrl, "qrCanvas", 60, 60)
+  //   if (state) {
+  //     let res = await this.canvasToTempImage()
+  //     var qrPath = res
+  //   }
+  //   console.log(qrPath)
+
+  //   try {
+  //     const info = wx.getSystemInfoSync()
+  //     var windowWidth = info.windowWidth
+  //     var windowHeight = info.windowHeight
+  //     this.setData({
+  //       windowHeight: windowHeight,
+  //       windowWidth: windowWidth
+  //     })
+  //   } catch (e) {
+  //     // Do something when catch error
+  //   }
+
+  //   const ctx = wx.createCanvasContext('myCanvas')
+  //   ctx.drawImage('../../img/post.jpg', 0, 0, windowWidth, windowHeight)
+  //   ctx.rect(windowWidth - 85, windowHeight - 105, 60, 60)
+  //   ctx.setFillStyle('red')
+  //   ctx.fill()
+  //   ctx.drawImage(qrPath, windowWidth - 85, windowHeight - 105, 60, 60)
+  //   ctx.draw()
+  // },
+  // createQrCode: function (url, canvasId, cavW, cavH) {
+  //   return new Promise((resolve, reject) => {
+  //     //调用插件中的draw方法，绘制二维码图片
+  //     QR.api.draw(url, canvasId, cavW, cavH);
+  //     resolve(true)
+  //   })
+  // },
   /**
    * 获取临时缓存照片路径，存入data中
    */
-  canvasToTempImage: function () {
-    return new Promise((resolve, reject) => {
-      //把当前画布指定区域的内容导出生成指定大小的图片，并返回文件路径。
-      wx.canvasToTempFilePath({
-        canvasId: 'qrCanvas',
-        success: function (res) {
-          var tempFilePath = res.tempFilePath;
-          resolve(tempFilePath)
-        },
-        fail: function (res) {
-          console.log(res);
-        }
-      });
-    })
-  },
+  // canvasToTempImage: function () {
+  //   return new Promise((resolve, reject) => {
+  //     //把当前画布指定区域的内容导出生成指定大小的图片，并返回文件路径。
+  //     wx.canvasToTempFilePath({
+  //       canvasId: 'qrCanvas',
+  //       success: function (res) {
+  //         var tempFilePath = res.tempFilePath;
+  //         resolve(tempFilePath)
+  //       },
+  //       fail: function (res) {
+  //         console.log(res);
+  //       }
+  //     });
+  //   })
+  // },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.init()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    this.init()
+
   },
 
   /**
