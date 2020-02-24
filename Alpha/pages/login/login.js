b2837a6ba820f3df5e65898c28da0dfcb8f74f0f
bupt3061 <bupt3061@163.com>
2020-02-24 23:35:23 +0800

    登陆

--- Alpha/pages/login/login.js
+++ Alpha/pages/login/login.js
@@ -1,4 +1,7 @@
 // pages/login/login.js
+const app = getApp()
+const zhenzisms = require('../../utils/zhenzisms.js');
+
 Page({
 
   /**
@@ -7,7 +10,9 @@ Page({
   data: {
     phone: null,
     code: null,
-    icon: 'password-view'
+    icon: 'password-view',
+    content: '发送验证码',
+    time: 60
   },
   /**
    * 其他函数
@@ -33,17 +38,61 @@ Page({
       phone: phone
     })
   },
-  getFocused: function() {
-    this.setData({
-      icon: "password-not-view"
-    })
-  },
   sendCode: function() {
-    const code = this.rand(1000, 9999)
-    console.log(code)
+    this.setData({
+      status: false
+    })
+    this.timer()
+    // const apiUrl = "https://sms_developer.zhenzikj.com"
+    // const appId = "104606"
+    // const appSecret = "1b219489-86f2-4119-8032-a0c8d2ef6105"
+
+    // // 获取电话
+    // var list = this.data.phone.split(' ')
+    // var phone = ''
+    // for(var i = 0; i < list.length; i++) {
+    //   phone += list[i]
+    // }
+    // console.log(phone)
+
+    // // 获取验证码
+    // const code = this.rand(1000, 9999)
+    // console.log(code)
+
+    // // 发送验证码
+    // zhenzisms.client.init(apiUrl, appId, appSecret);
+
+    // var params = {};
+    // params.number = phone;
+    // params.message = '您的验证码为:{' + code + '}，如非本人操作请忽略此短信';
+    // params.seconds = 60 * 5;
+    // params.length = 4;
+
+    // zhenzisms.client.sendCode(res => {
+    //   console.log(res.data)
+    // }, params);
+
+    // zhenzisms.client.balance(res => {
+    //   console.log(res.data)
+    // });
   },
   rand: function(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
+  },
+  timer: function() {
+    var time = this.data.time
+    time -= 1
+
+    if(time == 0) {
+      return
+    }
+
+    this.setData({
+      time: time
+    })
+
+    setTimeout(this.timer(), 1000)
+
   },
   /**
    * 生命周期函数--监听页面加载
