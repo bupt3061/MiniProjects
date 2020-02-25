4ce9bd4b7001dd72c6338ddc9dd9263a115c0e0d
bupt3061 <bupt3061@163.com>
2020-02-25 10:03:54 +0800

    2020/2/25 登陆

--- Alpha/pages/login/login.js
+++ Alpha/pages/login/login.js
@@ -9,90 +9,216 @@ Page({
    */
   data: {
     phone: null,
+    sysCode: null,
     code: null,
     icon: 'password-view',
     content: '发送验证码',
-    time: 60
+    time: 60,
+    idx: null,
+    flag: false,
+    status: true,
+    pBorderColor: "#f1f1f1",
+    cBorderColor: "#f1f1f1"
   },
   /**
    * 其他函数
    */
   inputPhone: function(e) {
     var res = e.detail.value
-    var phone = ''
-    var list = res.split(' ')
+    var phone = this.getPhone(res)
+    phone = this.processPhone(phone)
 
-    for (var i = 0; i < list.length; i++) {
-      phone += list[i]
+    console.log('phone', phone)
+
+    this.setData({
+      phone: phone
+    })
+  },
+  focusPhone: function() {
+    this.setData({
+      pBorderColor: "#14d1b5"
+    })
+  },
+  blurPhone: function() {
+    this.setData({
+      pBorderColor: "#f1f1f1"
+    })
+  },
+  inputCode: function(e) {
+    const code = e.detail.value
+    console.log('code', code)
+
+    if(code.length != 0) {
+      this.setData({
+        code: code,
+        icon: "password-not-view"
+      })
+    } else {
+      this.setData({
+        icon: "password-view"
+      })
+    }
+  },
+  focusCode: function () {
+    this.setData({
+      cBorderColor: "#14d1b5"
+    })
+  },
+  blurCode: function() {
+    this.setData({
+      cBorderColor: "#f1f1f1"
+    })
+  },
+  sendCode: function() {
+    const that = this
+
+    // 获取并验证电话
+    var phone = this.data.phone
+    phone = this.getPhone(phone)
+
+    var flag = this.checkPhone(phone)
+    console.log(flag)
+
+    if (!flag) {
+      console.log("电话号码错误")
+
+      return
     }
 
+    console.log("phone", phone)
+
+    // 倒计时
+    this.setData({
+      status: false
+    })
+
+    this.timer()
+
+    // 获取验证码
+    const sysCode = this.rand(1000, 9999)
+    console.log("sysCode", sysCode)
+
+    this.setData({
+      sysCode: sysCode
+    })
+
+    // 发送短信
+    const apiUrl = "https://sms_developer.zhenzikj.com"
+    const appId = "104606"
+    const appSecret = "1b219489-86f2-4119-8032-a0c8d2ef6105"
+
+    // 发送验证码
+    zhenzisms.client.init(apiUrl, appId, appSecret);
+
+    var params = {};
+    params.number = phone;
+    params.message = '您的验证码为:{' + sysCode + '}，如非本人操作请忽略此短信';
+    params.seconds = 60 * 5;
+    params.length = 4;
+
+    zhenzisms.client.sendCode(res => {
+      console.log(res.data)
+    }, params);
+
+    zhenzisms.client.balance(res => {
+      console.log(res.data)
+    });
+  },
+  rand: function(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  },
+  timer: function() {
+    const flag = this.data.flag
+    var time = this.data.time
+
+    // 设置计时器并存储到全局
+    var idx = setTimeout(this.timer, 1000)
+
+    if(!flag) {
+      this.setData({
+        idx: idx,
+        flag: false
+      })
+    }
+
+    // 到时重新启用
+    if (time == 0) {
+      const idx = this.data.idx
+
+      this.setData({
+        status: true,
+        flag: false,
+        content: "重新发送",
+        time: 60
+      })
+
+      clearTimeout(idx)
+    } else {
+      time = time - 1
+      this.setData({
+        time: time
+      })
+    }
+  },
+  processPhone: function(phone) {
     if (phone.length > 3 && phone.length <= 7) {
       phone = phone.slice(0, 3) + ' ' + phone.slice(3, phone.length)
     } else if (phone.length > 7) {
       phone = phone.slice(0, 3) + ' ' + phone.slice(3, 7) + ' ' + phone.slice(7, phone.length)
     }
 
-    console.log(phone)
-
-    this.setData({
-      phone: phone
-    })
+    return phone
   },
-  sendCode: function() {
-    this.setData({
-      status: false
-    })
-    this.timer()
-    // const apiUrl = "https://sms_developer.zhenzikj.com"
-    // const appId = "104606"
-    // const appSecret = "1b219489-86f2-4119-8032-a0c8d2ef6105"
-
-    // // 获取电话
-    // var list = this.data.phone.split(' ')
-    // var phone = ''
-    // for(var i = 0; i < list.length; i++) {
-    //   phone += list[i]
-    // }
-    // console.log(phone)
-
-    // // 获取验证码
-    // const code = this.rand(1000, 9999)
-    // console.log(code)
-
-    // // 发送验证码
-    // zhenzisms.client.init(apiUrl, appId, appSecret);
-
-    // var params = {};
-    // params.number = phone;
-    // params.message = '您的验证码为:{' + code + '}，如非本人操作请忽略此短信';
-    // params.seconds = 60 * 5;
-    // params.length = 4;
-
-    // zhenzisms.client.sendCode(res => {
-    //   console.log(res.data)
-    // }, params);
-
-    // zhenzisms.client.balance(res => {
-    //   console.log(res.data)
-    // });
-  },
-  rand: function(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  },
-  timer: function() {
-    var time = this.data.time
-    time -= 1
-
-    if(time == 0) {
-      return
+  getPhone: function(phone) {
+    if(!phone) {
+      return ''
     }
 
-    this.setData({
-      time: time
-    })
+    var res = ''
+    var list = phone.split(' ')
 
-    setTimeout(this.timer(), 1000)
+    for(var i = 0; i < list.length; i++) {
+      res += list[i]
+    }
 
+    return res
+  },
+  checkPhone: function(phone) {
+    var res = null
+    let reg = /^1(3[0-9]|4[5,7]|5[0,1,2,3,5,6,7,8,9]|6[2,5,6,7]|7[0,1,7,8]|8[0-9]|9[1,8,9])\d{8}$/;
+
+    if(!phone || phone == '') {
+      res = false
+    } else {
+      res = reg.test(phone)
+    }
+
+    if(!res) {
+      this.setData({
+        pBorderColor: "#e64240"
+      })
+    }
+
+    return res
+  },
+  checkCode: function() {
+    const code = this.data.code
+    const sysCode = this.data.sysCode
+    var flag = null
+
+    if(!code && !sysCode) {
+      flag = false
+    }
+
+    if(code != sysCode) {
+      flag = false
+    }
+
+    if(!flag) {
+      this.setData({
+        cBorderColor: "#e64240"
+      })
+    }
   },
   /**
    * 生命周期函数--监听页面加载
