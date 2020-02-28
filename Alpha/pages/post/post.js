3e1f02f7c89b07dfa1ea2fbd242fa0dc3ca1f93e
bupt3061 <bupt3061@163.com>
2020-02-28 11:18:50 +0800

    2020/2/28 海报

--- Alpha/pages/post/post.js
+++ Alpha/pages/post/post.js
@@ -11,42 +11,30 @@ Page({
     navigationHeight: null,
     screenWidth: null,
     windowHeight: null,
-    courseid: null
+    courseid: null,
+    state: null
   },
   /**
    * 初始化函数
    */
   async init() {
+    let state = await this.createQRCode()
+    console.log(state)
+
+    this.setData({
+      state: state
+    })
+  },
+  /**
+   * 其他函数
+   */
+  async drawCanvas() {
+    const state = this.data.state
     const courseid = this.data.courseid
     const screenWidth = this.data.screenWidth
     const windowHeight = this.data.windowHeight
     const navigationHeight = this.data.navigationHeight
-    const QRCode = require('weapp-qrcode')
-
-    // 绘制二维码
-    QRCode({
-      width: screenWidth * 0.6,
-      height: screenWidth * 0.6,
-      canvasId: 'qrcode',
-      text: courseid
-    })
-
-    this.drawQRCode()
-
-    // 获取课程名
-    const courses = app.globalData.courses
-
-    var coursename = null
-    for (var i = 0; i < courses.length; i++) {
-      if (courses[i]._id == courseid) {
-        coursename = courses[i].coursename
-        break
-      }
-    }
-    console.log(coursename)
-
-    // 随机获取文本
-    var texts = [
+    const texts = [
       "长风破浪会有时",
       "梅花香自苦寒来",
       "劝君惜取少年时",
@@ -55,15 +43,42 @@ Page({
       "观千剑而后识器"
     ]
 
-    var random = Math.floor(Math.random() * texts.length)
-    const text = texts[random]
+    let coursename
+    let text
+    let qrPath
 
-    // 绘制背景矩形
+    // 获取二维码地址
+    if (state) {
+      qrPath = await this.canvasToTempImage('qrCanvas')
+      console.log(qrPath)
+    }
+
+    // 获取课程名
+    const courses = app.globalData.courses
+
+    for (var i = 0; i < courses.length; i++) {
+      if (courses[i]._id == courseid) {
+        coursename = courses[i].coursename
+        break
+      }
+    }
+    console.log(coursename)
+
+    // 获取随机文本
+    var random = Math.floor(Math.random() * texts.length)
+    text = texts[random]
+    console.log(text)
+
+    /**
+     * 绘制canvas
+     */
+    // 创建上下文
     const ctx = wx.createCanvasContext('post')
+    // 绘制背景矩形
     ctx.setFillStyle('#14d1b5')
-    // 绘制圆角矩形
     ctx.fillRect(0, 0, screenWidth, windowHeight + navigationHeight)
-    this.roundRectColor(ctx, 24, 24, screenWidth - 48, windowHeight + navigationHeight - 48, 24)
+    // 绘制圆角矩形
+    this.roundRectColor(ctx, 24, 48, screenWidth - 48, windowHeight + navigationHeight - 48 - 48, 24)
     // 绘制圆
     ctx.beginPath();
     ctx.arc(24, (windowHeight + navigationHeight) * 0.66, 8, 0 * Math.PI / 180, 360 * Math.PI / 180);
@@ -78,21 +93,24 @@ Page({
     ctx.moveTo(42, (windowHeight + navigationHeight) * 0.66);
     ctx.lineTo(screenWidth - 24 - 16, (windowHeight + navigationHeight) * 0.66);
     ctx.stroke();
+    // 绘制二维码
+    ctx.drawImage(qrPath, screenWidth * 0.2, screenWidth * 0.2 + 24, screenWidth * 0.6, screenWidth * 0.6)
     // 绘制文本
-    ctx.font = '16px Arial';
+    ctx.font = '18px Arial';
     ctx.textAlign = 'center';
     ctx.fillStyle = '#000';
-    ctx.fillText(coursename, screenWidth / 2, (windowHeight + navigationHeight) * 0.33 + screenWidth * 0.3 + 40, screenWidth * 0.6);
+    ctx.fillText(coursename, screenWidth / 2, (windowHeight + navigationHeight) * 0.33 + (screenWidth * 0.4 + 12), screenWidth * 0.6);
     // 绘制文本
-    ctx.font = '20px Arial';
+    ctx.font = 'bold 20px Arial';
     ctx.textAlign = 'center';
     ctx.fillStyle = '#000';
-    ctx.fillText(text, screenWidth / 2, (windowHeight + navigationHeight) * 0.82, screenWidth * 0.6);
+    ctx.fillText(text, screenWidth / 2, (windowHeight + navigationHeight) * 0.8, screenWidth * 0.6);
     ctx.draw()
+
+    this.setData({
+      hidden: true
+    })
   },
-  /**
-   * 其他函数
-   */
   roundRectColor: function(context, x, y, w, h, r) { //绘制圆角矩形（纯色填充）
     context.save();
     context.setFillStyle("white");
@@ -104,18 +122,41 @@ Page({
     context.stroke();
     context.closePath();
   },
-  drawQRCode: function() {
-    // 绘制二维码
-    wx.canvasToTempFilePath({
-      canvasId: 'qrcode',
-      success: res => {
-        console.log(res)
-      },
-      fail: err => {
-        console.log(err)
-      }
-    }, this)
+  createQRCode: function() {
+    return new Promise((resolve, reject) => {
+      const courseid = this.data.courseid
+      const screenWidth = this.data.screenWidth
+      const windowHeight = this.data.windowHeight
+      const navigationHeight = this.data.navigationHeight
+      const QRCode = require('weapp-qrcode')
+
+      // 绘制二维码
+      QRCode({
+        width: screenWidth * 0.6,
+        height: screenWidth * 0.6,
+        canvasId: 'qrCanvas',
+        text: courseid
+      })
+
+      resolve(true)
+    })
   },
+  canvasToTempImage: function (canvasId) {
+    return new Promise((resolve, reject) => {
+      //把当前画布指定区域的内容导出生成指定大小的图片，并返回文件路径。
+      wx.canvasToTempFilePath({
+        canvasId: canvasId,
+        success: function (res) {
+          var tempFilePath = res.tempFilePath;
+          resolve(tempFilePath)
+        },
+        fail: function (res) {
+          console.log(res);
+        }
+      });
+    })
+  },
+  
   /**
    * 生命周期函数--监听页面加载
    */
@@ -124,7 +165,6 @@ Page({
     const screenWidth = app.globalData.screenWidth
     const windowHeight = app.globalData.windowHeight
     const navigationHeight = app.globalData.navigationHeight
-
 
     this.setData({
       screenWidth: screenWidth,
@@ -140,7 +180,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function() {
-
+    setTimeout(this.drawCanvas, 1000)
   },
 
   /**
